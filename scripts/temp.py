15e69bed7cc1a6fa03ae83046b8da2928390b7d2
rayane <rayane.merlin9@gmail.com>
2024-06-01 20:44:51 +0200

    move maps countries

--- scripts/temp.py
+++ scripts/temp.py
@@ -1,16 +1,5 @@
 import os
-import glob
-
-directories = glob.glob("public/images/maps/country/*")
 
 if __name__ == "__main__":
-    """ for path, subfolders, subfiles in os.walk("public/images/maps/country/*", topdown=False):
-        print(path, subfolders, subfiles) """
-    for directory in directories:
-        for _, _, subfiles in os.walk(directory, topdown=False):
-            # move vector.svg to ../
-            for subfile in subfiles:
-                if subfile.endswith(".svg"):
-                    print(subfile)
-                    os.rename(f"{directory}/{subfile}", f"public/images/maps/{subfile}")
-                    break
+    for _, _, subfiles in os.walk("public/images/maps/country", topdown=False):
+        print(subfiles)
