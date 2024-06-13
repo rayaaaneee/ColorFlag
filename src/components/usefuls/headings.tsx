3c91662bb3832fa7ccab2b80666070cf7a5eac9d
Rayane Merlin <rayane.merlin@icp-conseil.fr>
2024-06-13 14:24:00 +0200

    new packages

--- src/components/usefuls/headings.tsx
+++ src/components/usefuls/headings.tsx
@@ -7,26 +7,16 @@ export interface HeadingProps {
     colorClass?: string,
 }
 
-export const HeadingOne = ({ children = undefined, className = "", id = undefined, colorClass = "text-black" }: HeadingProps) => {
-    return (
-        <h1 id={id} className={`${className} ${colorClass} text-7xl mb-5`}>{ children }</h1>
-    )
-}
+export const HeadingOne = ({ children = undefined, className = "", id = undefined, colorClass = "text-black" }: HeadingProps) => 
+    (<h1 id={id} className={`${className} ${colorClass} text-7xl mb-5`}>{ children }</h1>);
 
-export const HeadingTwo = ({ children = undefined, className = "", id = undefined, colorClass = "text-slate-600"  }: HeadingProps) => {
-    return (
-        <h1 id={id} className={`${className}  ${colorClass} text-3xl mb-3`}>{ children }</h1>
-    )
-}
 
-export const HeadingThree = ({ children = undefined, className = "", id = undefined, colorClass = "text-slate-600" }: HeadingProps) => {
-  return (
-      <h1 id={id} className={`${className}  ${colorClass} text-xl mb-5`}>{ children }</h1>
-  )
-}
+export const HeadingTwo = ({ children = undefined, className = "", id = undefined, colorClass = "text-slate-600"  }: HeadingProps) => 
+    (<h1 id={id} className={`${className}  ${colorClass} text-3xl mb-3`}>{ children }</h1>);
 
-export const HeadingFour = ({ children = undefined, className = "", id = undefined, colorClass = "text-slate-600" }: HeadingProps) => {
-  return (
-      <h1 id={id} className={`${className}  ${colorClass} text-3xl mb-5`}>{ children }</h1>
-  )
-}
+
+export const HeadingThree = ({ children = undefined, className = "", id = undefined, colorClass = "text-slate-600" }: HeadingProps) => 
+    (<h1 id={id} className={`${className}  ${colorClass} text-xl mb-5`}>{ children }</h1>);
+
+export const HeadingFour = ({ children = undefined, className = "", id = undefined, colorClass = "text-slate-600" }: HeadingProps) => 
+    (<h1 id={id} className={`${className}  ${colorClass} text-3xl mb-5`}>{ children }</h1>);
